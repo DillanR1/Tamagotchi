e6a9b996a8312b5123e483563e8344fa611d292e
Dillan <dillanroby@yahoo.com>
2020-06-22 14:42:38 -0700

    Updated Wire Frame and User Story

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 
 
 class Tamagotchi
-    constructor(){
-
-
-
+    constructor(name, age = 0, hunger = 0,sleepiness = 0,boredom = 0){
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
     }
+
+
+    const startButton = document.getElementById('startGame');
+
+    startButton.addEventListener('click', handleStartGame);
+    
+    /* create function for timer
+    function handleStartGame () {
+        console.log('Start Game clicked!')
+
+        const timer = setInterval(function () {
+            time++;
+            console.log(time);
+            1000
+         })
+    }
+
+    */
