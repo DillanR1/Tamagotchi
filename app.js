6dd58973c8349f13ceeee83f9fe59daa7f4aebd4
Dillan <dillanroby@yahoo.com>
2020-06-24 14:30:03 -0700

    Getting Somewhere? Div class added, TODO list added

--- app.js
+++ app.js
@@ -8,67 +8,78 @@ class Tamagotchi {
         this.sleepiness = 0;
         this.boredom = 0;
   
-     startButton = document.getElementById
+      startButton = document.getElementById
         ('startGame');
 
      startButton.addEventListener('click',
      handleStartGame);   
 
-
-
-
-
-
-
-
+        // TODO ADD EVENT LISTENER FOR INTERACTIVE BUTTONS
+        // TODO ADD INTERACTIVE BUTTONS
+        // EVENT LISTENER WILL DECRIMENT
 
 
     
     function handleStartGame() {
         console.log('Game is Running!');
 
-      startTimer()
+      startHungerTimer()
+      //startSleepinessTimer()   
+      //startBoredomTimer()
+      //startAgeInterval()
     }
 
 
-    hungerTimer = function(){
+    startHungerTimer = function(){
         const timer = setInterval(function () {
             if (time > 0){
                 time--;
                 console.log(time);  
                 } else {
-                console.log("Timer Reset");
+                console.log('Hunger Timer Reset');
            }    clearInterval(timer);    
         
         }, 5000);
         
     }
-};
-   /*  sleepinessTimer = function(){
+}
+ 
+
+/*  sleepinessTimer = function(){
         const timer = setInterval(function () {
             if (time > 0){
                 time--;
                 console.log(time);
+                } else {  
+                console.log('Sleepiness Timer Reset');    
+           }    clearInterval(timer);
+        }, 9000);   
+    }    
+
 };
 
     
-    
-
-
      boredomTimer = function(){
         const timer = setInterval(function () {
             if (time > 0){
                 time--;
                 console.log(time);
+                } else {  
+                console.log('Boredom Timer Reset');    
+           }    clearInterval(timer);
+        }, 15000);   
+    }    
+
+};
+
+startAgeInterval
 
 
 
 
 
 
-
-
-
+// OLD CODE TO START TIMER
 
 
 // give constructor function default values 
@@ -93,17 +104,11 @@ const timer = setInterval(function ()
         hunger++}
         console.log(hunger); 
 
-        
-    //const startButton = document.getElementById('startGame');
 
-    //startButton.addEventListener('click', handleStartGame);
 
     /* NOTE pet age, hunger, sleepiness, and boredom need functions to increment
     once start game is pushed. They will appear as an alert to the player every minute. 
     The alerts should be randomized. */ 
 
 
-    
 
-
-    
