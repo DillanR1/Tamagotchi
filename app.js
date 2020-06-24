389046e1bd5ea099db8871a8e79a54938133cdfc
Dillan <dillanroby@yahoo.com>
2020-06-24 10:59:09 -0700

    fixed class constructor. Function placed between constructor and class

--- app.js
+++ app.js
@@ -1,47 +1,79 @@
 
 
 class Tamagotchi {
-    constructor  (name, age = 0, hunger = 0,sleepiness = 0,boredom = 0) {
+    constructor  (name) {
         this.name = name;
-        this.age = age; 
-        this.hunger = hunger|| (math.random <= .50) 
-            hunger++;
-             console.log(this.hunger);
-        
-             this.sleepiness = sleepiness|| (math.random <= .50) 
-        sleepiness++;
-         console.log(this.sleepiness)
-        
-         this.boredom = boredom|| (math.random <= .50) 
-        boredom++;
+        this.age = 0; 
+        this.hunger =0; 
+        this.sleepiness = 0;
+        this.boredom = 0;
+  
+    }
+     hungerTimer = function(){
+        const timer = setInterval(function () {
+            if (time > 0){
+                time--;
+                console.log(time);   //inside of set interval function, add interval
+                                     // google set interval syntax.
+            }
+        })
+
     }
 };
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+const tamagotchi1 = new Tamagotchi ("D")
+
+const tamagotchi2 = new Tamagotchi ("Yulia",0,0,0,0)
+
+// give constructor function default values 
+//create functions inside of class but OUTSIDE of constructor
+
+
+
+/*
+
+let time = 30;
+
+function hungerTimer(){
+    const timer = setInterval(function () {
+        if (time > 0) 
+            time--;
+            console.log(time);
+        }
+}
+const timer = setInterval(function () 
+        
+    if(time = 0) {hunger || (math.random <= .50); 
+        hunger++}
+        console.log(hunger); 
+*/
+        
     //const startButton = document.getElementById('startGame');
 
     //startButton.addEventListener('click', handleStartGame);
 
     /* NOTE pet age, hunger, sleepiness, and boredom need functions to increment
     once start game is pushed. They will appear as an alert to the player every minute. 
-    The alerts should be randomized. 
+    The alerts should be randomized. */ 
 
 
-    */
+    
 
 
-    /* create function for timer
-    function handleStartGame () {
-        console.log('Start Game clicked!')
-
-        const timer = setInterval(function () {
-            time++;
-            console.log(time);
-            1000
-         })
-    }
-
-    */
-
+     /*create function for timer
+        function handleStartGame () {
+            console.log('Start Game clicked!')
+                */
