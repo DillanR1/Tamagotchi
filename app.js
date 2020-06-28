8285c2ee04673c802665196b36a88cb5eefe8dfd
Dillan <dillanroby@yahoo.com>
2020-06-28 15:38:17 -0700

    Trying to Kill Pet

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("Hello!!!");
 
 class Tamagotchi {
     constructor  (name) {
@@ -13,11 +12,12 @@ class Tamagotchi {
     
 showPetInfo() {
     const displayDiv = document.getElementById(`petInfo`);
-    displayDiv.innerHTML = (`<p>Pet name: ${pet.name}</p>
-                             <p>Pet Age: ${pet.age}</p>
-                             <p>Pet Hunger: ${pet.hunger}</p>
-                             <p>Pet Sleepiness: ${pet.sleepiness}</p>
-                             <p>Pet Boredom: ${pet.boredom}</p>`);
+    displayDiv.innerHTML = 
+    (`<p>Pet name: ${pet.name}</p>
+    <p>Pet Age: ${pet.age}</p>
+    <p>Pet Hunger: ${pet.hunger}</p>
+    <p>Pet Sleepiness: ${pet.sleepiness}</p>
+    <p>Pet Boredom: ${pet.boredom}</p>`);
 };
 
     handleFeedPet() {
@@ -50,12 +50,27 @@ showPetInfo() {
     boredomTimer() {
             this.boredom++
             console.log('boredom: ', this.boredom);
-    };    
+    };   
+    
+    killPet() {
+        (this.hunger > 10 || this.sleepiness > 10 || this.boredom > 10)
+        clearInterval(petInstance)
+        startButton.disabled = true
+        feedButton.disabled = true
+        sleepButton.disabled = true
+        petPlayButton.disabled = true
+    };
+
+    morphPet() {
+        (age === 3)
+        (age === 6)
+        (age === 9)
+    }
 
 };
 
 function handleStartGame(petInstance) { 
-     setInterval(function () {
+        setInterval(function () {
         petInstance.showPetInfo()
         petInstance.hungerTimer()
         petInstance.sleepinessTimer()   
@@ -66,20 +81,20 @@ function handleStartGame(petInstance) {
 function handleStartAgeInterval(petInstance) {
       setInterval(function () {
         petInstance.startAgeInterval()
-      }, 6000);
+      }, 4000);
 };
 
 let petName = window.prompt('Name Your Pet!');
-
 const pet = new Tamagotchi(petName);
 
 const newDiv = document.createElement(`div`);
-
 newDiv.setAttribute(`id`, `petInfo`);
 
 document.body.appendChild(newDiv);
-
 const startButton = document.getElementById('startGame');
+
+
+
 
 startButton.addEventListener('click',
     function (){
@@ -112,7 +127,6 @@ const petPlayButton = document.getElementById
 petPlayButton.addEventListener('click',
     function (){
         pet.handlePetPlayButton()
-        500
     }
 );
 
