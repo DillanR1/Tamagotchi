262689df33fce87f629bf8db8ea0e5ca7ba050b9
Dillan <dillanroby@yahoo.com>
2020-07-01 20:20:10 -0700

    pet picture and background added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ class Tamagotchi {
        
     
 showPetInfo() {
+    if (this.hunger <= 11||this.sleepiness <= 11||this.boredom <= 11) {
     const displayDiv = document.getElementById(`petInfo`);
     displayDiv.innerHTML = 
   (`<p>Pet name: ${pet.name}</p>
@@ -18,7 +19,16 @@ showPetInfo() {
     <p>Pet Hunger: ${pet.hunger}</p>
     <p>Pet Sleepiness: ${pet.sleepiness}</p>
     <p>Pet Boredom: ${pet.boredom}</p>`);
-};
+    const displayImg = document.createElement(`img`);
+    newImg.setAttribute(`src`, 'https://assets.pokemon.com/assets/cms2/img/misc/countries/au/country_detail_pokemon.png');
+    document.body.appendChild(newImg);
+    
+   } else {
+   const deadImg = document.createElement(`img2`)
+   newImg2.setAttribute(`src`, 'https://www.deviantart.com/widheim/art/Dead-Pikachu-Drawing-715374200');
+   document.body.appendChild(newImg2);
+   }
+}
 
     handleFeedPet() {
     const feedButton = document.getElementById('feed'); 
@@ -68,7 +78,7 @@ showPetInfo() {
         if (this.boredom >= 11) {
             clearInterval(handleStartGame, 0);
             document.getElementById('play').disabled = true;
-            window.alert('Your pet ran away due to boredom');
+            window.alert('Your pet was bored to death');
             }else{
             this.boredom++
             console.log('boredom: ', this.boredom);
@@ -114,6 +124,15 @@ function handleStartAgeInterval(petInstance) {
 
 let petName = window.prompt('Name Your Pet!');
 const pet = new Tamagotchi(petName);
+
+const newImg = document.createElement(`img`);
+newImg.setAttribute(`src`, 'https://assets.pokemon.com/assets/cms2/img/misc/countries/au/country_detail_pokemon.png');
+document.body.appendChild(newImg);
+
+
+const newImg2 = document.createElement(`img2`)
+newImg2.setAttribute(`src`, 'https://https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5554f72f-5675-4504-aaff-fcc46bbb8817/dbtwy9k-7fa0f5f4-8b18-4e1f-8f8f-5ec4262cd2f9.jpg/v1/fill/w_1024,h_967,q_75,strp/dead_pikachu_drawing_by_widheim_dbtwy9k-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD05NjciLCJwYXRoIjoiXC9mXC81NTU0ZjcyZi01Njc1LTQ1MDQtYWFmZi1mY2M0NmJiYjg4MTdcL2RidHd5OWstN2ZhMGY1ZjQtOGIxOC00ZTFmLThmOGYtNWVjNDI2MmNkMmY5LmpwZyIsIndpZHRoIjoiPD0xMDI0In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.4mhzNU-FJZGB0mffg8Cn1DR0ooCiAvmMxeo015uqPsU');
+document.body.appendChild(newImg2);
 
 const newDiv = document.createElement(`div`);
 newDiv.setAttribute(`id`, `petInfo`);
